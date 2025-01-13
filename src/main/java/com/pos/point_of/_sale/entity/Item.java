0df8e24ee6cdd3015c1548ca3d76f32b37f7e29d
SirajDilshan <sirajdilshan789@gmail.com>
2025-01-13 11:32:36 +0530

    measuringunitType and item

---
+++ src/main/java/com/pos/point_of/_sale/entity/Item.java
@@ -0,0 +1,35 @@
+package com.pos.point_of._sale.entity;
+
+import com.pos.point_of._sale.entity.enums.MeasuringUnitType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "item")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Item {
+    @Id
+    @Column(name = "item_id",length = 45 , unique = true)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int itemId;
+
+    @Column(name = "item_name",length = 100,nullable = false)
+    private String itemName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "measuring_unit",length = 100,nullable = false)
+    private MeasuringUnitType measuringUnitType;
+
+    @Column(name = "balance_qty",length = 100,nullable = false)
+    private double balanceQty;
+
+    @Column(name = "supplier_price",length = 100,nullable = false)
+    private double supplierPrice;
+
+    @Column(name = "selling_price",length = 100,nullable = false)
+    private double sellingPrice;
+}
