d764dd5acbc583f854d82768bfd46ea73a3bbfd1
SirajDilshan <sirajdilshan789@gmail.com>
2025-01-13 11:33:46 +0530

    Cteate ItemServiceIMPL

---
+++ src/main/java/com/pos/point_of/_sale/service/impl/ItemServiceIMPL.java
@@ -0,0 +1,8 @@
+package com.pos.point_of._sale.service.impl;
+
+import com.pos.point_of._sale.service.ItemService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItemServiceIMPL implements ItemService {
+}
