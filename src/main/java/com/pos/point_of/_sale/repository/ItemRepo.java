7d8c07fec809215d8011b625a5b38308e5ee7337
SirajDilshan <sirajdilshan789@gmail.com>
2025-01-13 11:34:05 +0530

    create ItemRepo

---
+++ src/main/java/com/pos/point_of/_sale/repository/ItemRepo.java
@@ -0,0 +1,11 @@
+package com.pos.point_of._sale.repository;
+
+import com.pos.point_of._sale.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@EnableJpaRepositories
+public interface ItemRepo extends JpaRepository<Item,Integer> {
+}
