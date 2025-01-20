8967420ba7344311c47f592754bb568ed3d7ded4
SirajDilshan <sirajdilshan789@gmail.com>
2025-01-20 20:34:21 +0530

    item service

--- src/main/java/com/pos/point_of/_sale/service/ItemService.java
+++ src/main/java/com/pos/point_of/_sale/service/ItemService.java
@@ -1,7 +1,18 @@
 package com.pos.point_of._sale.service;
 
+import com.pos.point_of._sale.dto.ItemDTO;
 import com.pos.point_of._sale.dto.request.ItemSaveRequestDTO;
+
+import java.util.List;
 
 public interface ItemService {
     String addItem(ItemSaveRequestDTO itemSaveRequestDTO);
+
+
+    List<ItemDTO> getAllItem();
+
+    List<ItemDTO> getAllItemsByStateType(boolean status);
+
+
+    int countALLItems();
 }
