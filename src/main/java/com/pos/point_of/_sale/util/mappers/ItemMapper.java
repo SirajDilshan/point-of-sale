03a6935e9739aaedf54c4d9721eeb990055cb32f
SirajDilshan <sirajdilshan789@gmail.com>
2025-01-23 19:13:09 +0530

    map ItemDTO to as PageDtolist

--- src/main/java/com/pos/point_of/_sale/util/mappers/ItemMapper.java
+++ src/main/java/com/pos/point_of/_sale/util/mappers/ItemMapper.java
@@ -1,9 +1,14 @@
 package com.pos.point_of._sale.util.mappers;
 
+import com.pos.point_of._sale.dto.ItemDTO;
+import com.pos.point_of._sale.dto.peginated.PaginatedResponseItemDTO;
 import com.pos.point_of._sale.dto.request.ItemSaveRequestDTO;
 import com.pos.point_of._sale.entity.Item;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ItemMapper {
@@ -12,4 +17,8 @@ public interface ItemMapper {
     @Mapping(target = "itemId", ignore = true)  // Since it's auto-generated
     @Mapping(target = "activeState", ignore = true)
     Item RequestDtoToEntity(ItemSaveRequestDTO itemSaveRequestDTO);
+
+    List<ItemDTO> pageToList(Page<Item>page);
+
+
 }
